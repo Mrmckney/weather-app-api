f2a7d60a9999c1533fc48cb1cd8e1efaff5c1132
mrmckney <mrmckney@gmail.com>
2023-05-15 23:41:28 -0400

    initial commit

---
+++ api.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import { Request, Response } from "express"
+import fetch from "node-fetch"
+import * as dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/api/weather", (req: Request, res: Response) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.long}&units=imperial&appid=${process.env.VITE_API_KEY}`)
+        .then(async (response) => {
+            const data = await response.json()
+            res.json(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.post("/api/forecast", (req: Request, res: Response) => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${req.body.lat}&lon=${req.body.long}&units=imperial&appid=${process.env.VITE_API_KEY}`)
+        .then(async (response) => {
+            const data = await response.json()
+            res.json(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.post("/api/search", (req: Request, res: Response) => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${req.body.word}&limit=5&appid=${process.env.VITE_API_KEY}`)
+        .then(async (response) => {
+            const data = await response.json()
+            res.json(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.listen(process.env.VITE_PORT, () => console.log(`Server on port ${process.env.VITE_PORT || 3001}`))
